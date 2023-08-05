fdb81feed951d7569150d4c9864257d76ea0c1db
benjyalper <123838171+benjyalper@users.noreply.github.com>
2023-08-05 21:01:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ $(".grid-item").on("click", function(){
     $('.grid-item').addClass('animate__flipInY');
     $('.grid-item').css('background-color', 'green');
     document.write(this);
+    var nthChild = $(this).index() + 1;
+      console.log("Clicked item's nth child:", nthChild);
 });
 
